bb7068ecd8bff11c19d4282c03e0cfe9b9e086c6
Lukas Vukovich <lukas.vukovich@gmail.com>
2020-06-09 18:07:18 -0400

    Added method titles which need to be filled in.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,52 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = '';
+  }
+
+  createPhrases() {
+    const arr = [
+      new Phrase('Life if like a box of chocolates'),
+      new Phrase('You shall not pass'),
+      new Phrase('The things you own end up owning you'),
+      new Phrase('Are you not entertained'),
+      new Phrase('This is sparta'),
+    ];
+    return arr;
+  }
+
+  getRandomPhrase() {
+    const randomPhrase = this.phrases[
+      Math.floor(Math.random() * this.phrases.length)
+    ];
+    return randomPhrase;
+  }
+
+  startGame() {
+    document.getElementById('overlay').style.display = 'none';
+
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+  }
+
+  handleInteraction() {}
+
+  checkForWin() {
+    //This method checks to see if the player has revealed all of the letters in the active phrase.
+  }
+
+  removeLife() {
+    //This method removes a life from the scoreboard, by replacing one
+    // of the `liveHeart.png` images with a `lostHeart.png` image (found in the `images`
+    // folder) and increments the `missed` property. If the player has five missed
+    // guesses (i.e they're out of lives), then end the game by calling the `gameOver()` method.
+  }
+
+  gameOver() {
+    //This method displays the original start screen overlay, and
+    // depending on the outcome of the game, updates the overlay `h1` element with a
+    // friendly win or loss message, and replaces the overlay’s `start` CSS class with
+    // either the `win` or `lose` CSS class.
+  }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,32 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  addPhraseToDisplay() {
+    const ul = document.getElementById('phrase').firstElementChild;
+    [...this.phrase].map((character) => {
+      const li = document.createElement('li');
+      if (character != ' ') {
+        li.className = `hide letter ${character}`;
+        li.innerText = character;
+        ul.appendChild(li);
+      } else {
+        li.className = 'space';
+        li.innerText = ' ';
+        ul.appendChild(li);
+      }
+    });
+  }
+
+  checkLetter() {
+    // Checks to see if letter selected by the player matches a letter in the phrase
+  }
+
+  showMatchedLetter() {
+    // Reveals the letter(s) on the board that matches the
+    // player's selection. To reveal the matching letter(s), select all of the letter DOM
+    // elements that have a CSS class name that matches the selected letter and
+    // replace each selected element's `hide` CSS class with the `show` CSS class.
+  }
+}
